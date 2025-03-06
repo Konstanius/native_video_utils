7c0a4c883ef5e24b221ca4438fbffe9e80959edd
Konstanius <earlygamerhd@gmail.com>
2025-03-06 15:21:43 +0100

    Experimental iOS fix #4

--- ios/Classes/NativeVideoUtilsPlugin.swift
+++ ios/Classes/NativeVideoUtilsPlugin.swift
@@ -9,19 +9,19 @@ public class NativeVideoUtilsPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if let args = args as? Dictionary<String, Any> {
+    if let args = call.arguments as? Dictionary<String, Any> {
       switch call.method {
       case "trimVideo":
-        let inputPath = args("inputPath")!
-        let outputPath = args("outputPath")!
-        let start = Int(args("startMs")!)!
-        let end = Int(args("endMs")!)!
+        let inputPath = args["inputPath"]!
+        let outputPath = args["outputPath"]!
+        let start = Int(args["startMs"]!)!
+        let end = Int(args["endMs"]!)!
 
         result(VideoUtils.trimVideo(srcPath: inputPath, dstPath: outputPath, startMs: start, endMs: end))
       case "rotateVideo":
-        let inputPath = args("inputPath")!
-        let outputPath = args("outputPath")!
-        let rotation = Int(args("rotationSteps")!)!
+        let inputPath = args["inputPath"]!
+        let outputPath = args["outputPath"]!
+        let rotation = Int(args["rotationSteps"]!)!
 
         result(VideoUtils.rotateVideo(srcPath: inputPath, dstPath: outputPath, rotationSteps: rotation))
       default:
